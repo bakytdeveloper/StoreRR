9e401df49f62bb8d778cba1dab4e0ffd961aecd5
bakytdeveloper <bakytdeveloper@gmail.com>
2023-12-10 19:20:25 +0600

    Расширил модель, для второстепенных картинок

--- client/src/components/ProductCard/ProductCard.js
+++ client/src/components/ProductCard/ProductCard.js
@@ -16,3 +16,5 @@ const ProductCard = ({ product }) => {
 };
 
 export default ProductCard;
+
+
--- server/models/Product.js
+++ server/models/Product.js
@@ -1,3 +1,21 @@
+// const mongoose = require('mongoose');
+//
+// const productSchema = new mongoose.Schema({
+//     name: { type: String, required: true },
+//     description: { type: String, required: true },
+//     type: { type: String, required: true },
+//     price: { type: Number, required: true },
+//     image: { type: String, required: true }, // URL к изображению товара
+//     specifications: [{ key: String, value: String }], // Характеристики товара в виде таблицы
+// });
+//
+// const Product = mongoose.model('Product', productSchema);
+//
+// module.exports = Product;
+
+
+
+
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
@@ -5,7 +23,8 @@ const productSchema = new mongoose.Schema({
     description: { type: String, required: true },
     type: { type: String, required: true },
     price: { type: Number, required: true },
-    image: { type: String, required: true }, // URL к изображению товара
+    image: { type: String, required: true }, // URL к основному изображению товара
+    secondaryImages: [{ type: String }], // Массив URL-ов второстепенных изображений товара
     specifications: [{ key: String, value: String }], // Характеристики товара в виде таблицы
 });
 
